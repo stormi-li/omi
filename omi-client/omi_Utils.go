7efd9ba7db1570a7083f640f54a6bb1773c314f3
Your Name <your.email@example.com>
2024-11-05 22:22:06 +0800

    refine code

--- omi-client/omi_Utils.go
+++ omi-client/omi_Utils.go
@@ -22,23 +22,18 @@ func getKeysByNamespace(redisClient *redis.Client, namespace string) []string {
 	var keys []string
 	cursor := uint64(0)
 	for {
-		// 使用 SCAN 命令获取键名
 		res, newCursor, err := redisClient.Scan(context.Background(), cursor, namespace+"*", 0).Result()
 		if err != nil {
 			return nil
 		}
-		// 处理键名，去掉命名空间
 		for _, key := range res {
-			// 去掉命名空间部分
 			keyWithoutNamespace := key[len(namespace):]
 			keys = append(keys, keyWithoutNamespace[1:])
 		}
 		cursor = newCursor
-		// 如果游标为0，则结束循环
 		if cursor == 0 {
 			break
 		}
 	}
-
 	return keys
 }
