e09853c2cb2b520e63f0f4f721108156d73852dc
Your Name <your.email@example.com>
2024-11-12 02:18:56 +0800

    commit

--- test/test.go
+++ test/test.go
@@ -5,6 +5,12 @@ import (
 	"github.com/stormi-li/omi"
 )
 
+var redisAddr = "118.25.196.166:3934"
+var password = "12982397StrongPassw0rd"
+
 func main() {
-	omi.NewWebClient(&redis.Options{})
+	web := omi.NewWebClient(&redis.Options{Addr: redisAddr, Password: password})
+	web.GenerateTemplate()
+	webServer := web.NewWebServer("web demo", 1)
+	webServer.Listen("118.25.196.166:8848")
 }
