6b7ebfe4a7108c5f25ac51b4082f8d8d74000539
Your Name <your.email@example.com>
2024-11-12 15:47:15 +0800

    refine index.html

--- omi-web/demo-server/demo.go
+++ omi-web/demo-server/demo.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/stormi-li/omi"
 )
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("/request")
-	var str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-	fmt.Fprintln(w, "Hello World send by http"+str)
-}
-
 var redisAddr = "118.25.196.166:3934"
 var password = "12982397StrongPassw0rd"
 
 func main() {
-	omi.NewServerManager(&redis.Options{Addr: redisAddr, Password: password}).NewRegister("helloworldserver", 1).Register("118.25.196.166:8081")
+	serverManager := omi.NewServerManager(&redis.Options{Addr: redisAddr, Password: password})
+	register := serverManager.NewRegister("hello_server", 1)
+	register.Register("118.25.196.166:8081")
 
-	http.HandleFunc("/request1", requestHandler) // 注册 /request 路径的处理函数
-	fmt.Println("Server is listening on :8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello", r.URL.Query().Get("name"), ", welcome to use omi")
+	})
+	http.ListenAndServe(":8081", nil)
 }
